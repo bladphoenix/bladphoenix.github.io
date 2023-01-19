3741ae59ea0c7978ff28b71af3b244c92c05c93a
gigihling <gigih.dvlpr@gmail.com>
2023-01-19 13:21:07 +0700

    update cdn materialize link

--- index.php
+++ index.php
@@ -5,7 +5,10 @@
    <!--Import Google Icon Font-->
    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
    <!--Import materialize.css-->
-   <link type="text/css" rel="stylesheet" href="css/materialize.min.css" media="screen,projection" />
+   <!-- <link type="text/css" rel="stylesheet" href="css/materialize.min.css" media="screen,projection" /> -->
+
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+
    <link rel="stylesheet" type="text/css" href="css/style.css">
 
    <!--Let browser know website is optimized for mobile-->
@@ -334,7 +337,10 @@
 
 
    <!--JavaScript at end of body for optimized loading-->
-   <script type="text/javascript" src="js/materialize.min.js"></script>
+   <!-- <script type="text/javascript" src="js/materialize.min.js"></script> -->
+   <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+
+
    <script type="text/javascript">
      const sideNav = document.querySelectorAll('.sidenav');
      M.Sidenav.init(sideNav);
